f474790b1824d52d0f045eb0c5e5eeb477c07fa9
ace <akhilratnsingh@gmail.com>
2022-06-09 23:59:01 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['*'],
+  theme: {
+    fontFamily:{
+      'nunito':['Nunito', 'sans-serif']
+    },
+    extend: {
+      screens:{
+        "sm":"480px"
+      },
+      spacing:{
+        'big':'48rem'
+      }
+    },
+  },
+  plugins: [],
+}
